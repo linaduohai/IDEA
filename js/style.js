88e2ddec635bc58313982fd27fb8aef4f13d3a79
mingfei <mingfei.net@gmail.com>
2015-04-09 22:24:34 +0800

    edit style.js
    att;

--- js/style.js
+++ js/style.js
@@ -2,4 +2,7 @@ $(function () {
     $('tr td:nth-child(1)').addClass("key");
     $('tr td:nth-child(2)').addClass("english");
     $('tr td:nth-child(3)').addClass("chinese");
+    $('img').click(function () {
+        $('tr td:nth-child(2)').fadeToggle();
+    });
 });
